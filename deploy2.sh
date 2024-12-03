fb96d621059c4f1dc2164e42d4d426a645f7e02b
Tom <adamkindo09@gmail.com>
2024-12-02 22:20:01 -0600

    testing38

--- deploy2.sh
+++ deploy2.sh
@@ -1,2 +1,3 @@
+Developpers, please know we are here to assist
 git is a gift when it comes to distribution and data security 
-development must be reviewed by managers `:wq!before deployment
+development must be reviewed by managers before deployment
