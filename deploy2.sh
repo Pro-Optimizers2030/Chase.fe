c8dd2dcce05458936f118d1a547b1497a21e6a92
DeVops Engineer <adamkindo09@gmail.com>
2024-12-05 10:01:05 -0600

    Update deploy2.sh

--- deploy2.sh
+++ deploy2.sh
@@ -1,3 +1,4 @@
 Developpers, please know we are here to assist
 git is a gift when it comes to distribution and data security 
 development must be reviewed by managers before deployment
+Maven is the acculator of knowledge
